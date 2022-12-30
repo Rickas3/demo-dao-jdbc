35f31f23dd70671b010c3268b62fab13550bf260
Ricardo Silva <rickas3@hotmail.com>
2022-12-30 12:12:41 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,7 +24,11 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		System.out.println("\n=== TEST 3: Seller findALL =====");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 		
 		
 		
